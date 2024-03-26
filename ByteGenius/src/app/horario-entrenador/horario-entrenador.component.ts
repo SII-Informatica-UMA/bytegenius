a2d393ef723a11ad0e8d27850843fd7135d062b2
VictorRueda <victor2rg@uma.es>
2024-03-26 20:09:25 +0100

    Victor -> Primer commit

--- ByteGenius/src/app/horario-entrenador/horario-entrenador.component.ts
+++ ByteGenius/src/app/horario-entrenador/horario-entrenador.component.ts
@@ -1,29 +1,25 @@
 import { Component } from '@angular/core';
-import { AppComponent } from '../app.component';
+import { CommonModule } from '@angular/common';
 import {UsuarioService } from './horario-entrenador.service';
 import { Dia } from '../Dia';
-import { CommonModule } from '@angular/common';
-import { NgFor } from '@angular/common';
 import { Hora } from '../Hora';
 
 @Component({
   selector: 'app-horario-entrenador',
   standalone: true,
-  imports: [CommonModule,NgFor],
+  imports: [CommonModule],
   templateUrl: './horario-entrenador.component.html',
   styleUrl: './horario-entrenador.component.css'
 })
+
 export class HorarioEntrenadorComponent {
   dias: Dia [] = [];
   horas: Hora [] = [];
 
   constructor(private usuariosservice: UsuarioService) { }
 
-
   ngOnInit(): void {
     this.dias = this.usuariosservice.getDias();
   }
-
-
 
 }
