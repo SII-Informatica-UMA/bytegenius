b2978cda74787435a39d977eeed50ca2afa05246
Juan Diego Alba Cantero <juandiegoalbcan@gmail.com>
2024-03-29 13:18:21 +0100

    pequeños cambios

--- ByteGenius/src/app/horario-cliente/horario-cliente.component.ts
+++ ByteGenius/src/app/horario-cliente/horario-cliente.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ScheduleModule, RecurrenceEditorModule, DayService, WeekService, WorkWeekService, MonthService, MonthAgendaService} from '@syncfusion/ej2-angular-schedule';
+import { CommonModule } from '@angular/common';
+import { HorarioClienteComponent } from './horario-cliente/horario-cliente.component';
 
 @Component({
   selector: 'app-horario-cliente',
@@ -10,5 +12,11 @@ import { ScheduleModule, RecurrenceEditorModule, DayService, WeekService, WorkWe
   styleUrl: './horario-cliente.component.css'
 })
 export class HorarioClienteComponent {
-
+  daysOfWeek = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+  trainers = [
+  {name: 'Entrenador 1'},
+  {name: 'Entrenador 2'},
+  {name: 'Entrenador 3'},
+  // ...
+];
 }
