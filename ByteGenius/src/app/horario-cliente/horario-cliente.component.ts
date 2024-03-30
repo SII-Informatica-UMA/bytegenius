944eec624174630e45e3e9045ed98812d993d29d
jaimegarfia <jaimegarfia@uma.es>
2024-03-30 20:38:08 +0100

    Boton de reservar hecho

--- ByteGenius/src/app/horario-cliente/horario-cliente.component.ts
+++ ByteGenius/src/app/horario-cliente/horario-cliente.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ScheduleModule, RecurrenceEditorModule, DayService, WeekService, WorkWeekService, MonthService, MonthAgendaService} from '@syncfusion/ej2-angular-schedule';
 import { CommonModule } from '@angular/common';
-import { HorarioClienteComponent } from './horario-cliente/horario-cliente.component';
+
 
 @Component({
   selector: 'app-horario-cliente',
@@ -11,7 +11,13 @@ import { HorarioClienteComponent } from './horario-cliente/horario-cliente.compo
   templateUrl: './horario-cliente.component.html',
   styleUrl: './horario-cliente.component.css'
 })
-export class HorarioClienteComponent {
+export class HorarioClienteComponent implements OnInit {
+  currentDay!: number;
+  mostrarCalendarioFlag: boolean = false;
+  constructor() { }
+  ngOnInit(): void {
+    this.currentDay = new Date().getDate();
+  }
   daysOfWeek = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
   trainers = [
   {name: 'Entrenador 1'},
@@ -19,4 +25,22 @@ export class HorarioClienteComponent {
   {name: 'Entrenador 3'},
   // ...
 ];
+
+getDayClass(dayNumber: number): number {
+  switch (dayNumber) {
+    case 1: return this.currentDay-5;
+    case 2: return this.currentDay-4;
+    case 3: return this.currentDay-3;
+    case 4: return this.currentDay-2;
+    case 5: return this.currentDay-1;
+    case 6: return this.currentDay;
+    case 7: return this.currentDay+1;
+    default: return 0;
+  }
+}
+
+toggleCalendario(day: number): void {
+  this.mostrarCalendarioFlag = !this.mostrarCalendarioFlag;
+}
+
 }
