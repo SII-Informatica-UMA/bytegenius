c95d55e4fe3ea65e4f99ba990e5f81d0c90ad654
Pablo6rubio <pablomunoz@uma.es>
2024-04-08 17:32:37 +0200

    Añado la funcion para seleccionar semana

--- ByteGenius/src/app/datepicker/datepicker.component.ts
+++ ByteGenius/src/app/datepicker/datepicker.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { NgbCalendar, NgbDate, NgbDatepickerModule, NgbDateStruct, NgbDateStructAdapter } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
 import { setYear } from 'date-fns';
@@ -11,20 +12,33 @@ import { setYear } from 'date-fns';
 	templateUrl: './datepicker.component.html',
 })
 export class NgbdDatepickerBasic {
-	today = inject(NgbCalendar).getToday();
-	
-
+	today: NgbDate;
+	di: String = '';
+  
 	model: NgbDateStruct;
 	date: { year: number; month: number };
+  
+	constructor(private calendar: NgbCalendar, private parserFormatter: NgbDateParserFormatter) {
+		// Inicialización de las propiedades
+		this.today = this.calendar.getToday();
+		this.model = { year: this.today.year, month: this.today.month, day: this.today.day };
+		this.date = { year: this.today.year, month: this.today.month };
+	  }
+	
+	  rellenarSemana() {
+		const dia: NgbDate = new NgbDate(this.date.year, this.date.month, this.today.day);
+		// Hacer algo con 'dia'
+	  }
 
-  constructor() {
-    // Inicialización de las propiedades
-    this.model = { year: this.today.year, month: this.today.month, day: this.today.day };
-    this.date = { year: this.today.year, month: this.today.month };
-  }
+	  ngMode(){
 
-  rellenarSemana(){
- 	const dia:NgbDate = new NgbDate(this.date.year,this.date.month,this.today.day);
-  }
+	  }
+	
+	  onDateSelection(event: any) {
+		// Extrae la fecha del evento
+		const selectedDate: NgbDate = event;
+		// Asigna el día seleccionado a 'di'
+		this.di = selectedDate.day.toString() + selectedDate.month; 
+	  }
 
 }
