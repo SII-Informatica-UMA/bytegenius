54cfaae07792ea77362c588b1186f84ccb2c3e34
Pablo6rubio <pablomunoz@uma.es>
2024-05-13 18:39:06 +0200

    Creo NuevoEventoDTO

---
+++ ByteGenius T2/src/main/java/ByteGenius/tarea2/dtos/EventoNuevoDTO.java
@@ -0,0 +1,23 @@
+package ByteGenius.tarea2.dtos;
+
+import java.util.Date;
+
+import ByteGenius.tarea2.entities.Tipo;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EventoNuevoDTO {
+    private String nombre;
+    private String descripción;
+    private String observaciones;
+    private String lugar;
+    private Integer duracionMinutos;
+    private Date inicio;
+    private String reglaRecurrencia;
+    private Integer idCliente;
+    private Tipo tipo;
+}
