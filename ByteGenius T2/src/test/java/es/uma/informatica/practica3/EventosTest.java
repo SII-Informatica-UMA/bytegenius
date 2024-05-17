f638e73d2f509aea07dad8a31b5513f8a890af0e
Pablo6rubio <116959033+Pablo6rubio@users.noreply.github.com>
2024-05-17 16:37:55 +0200

    Añado dependencias pero sigue sin funcvonalr

--- ByteGenius T2/src/test/java/es/uma/informatica/practica3/EventosTest.java
+++ ByteGenius T2/src/test/java/es/uma/informatica/practica3/EventosTest.java
@@ -3,8 +3,6 @@ package es.uma.informatica.practica3;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.net.URI;
-import java.util.Collections;
-import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -24,83 +22,75 @@ import org.springframework.web.util.UriBuilderFactory;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.MediaType;
 
-
-
 import ByteGenius.tarea2.repositories.EventoRepository;
+import ByteGenius.tarea2.Application; // Asegúrate de importar tu clase de aplicación principal
+import ByteGenius.tarea2.entities.Evento;
 
-
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@DisplayName("En el servicio de productos")
+@SpringBootTest(classes = Application.class, webEnvironment = WebEnvironment.RANDOM_PORT)
+@DisplayName("En el servicio de calendario")
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 class EventosTest {
 
-	@Autowired
-	private TestRestTemplate restTemplate;
+    @Autowired
+    private TestRestTemplate restTemplate;
 
-	@Value(value="${local.server.port}")
-	private int port;
+    @Value(value="${local.server.port}")
+    private int port;
 
-	@Autowired
-	private EventoRepository eventoRepository;
+    @Autowired
+    private EventoRepository eventoRepository;
 
     @BeforeEach
     public void initializeDatabase() {
         eventoRepository.deleteAll();
     }
 
-	private URI uri(String scheme, String host, int port, String ...paths) {
-		UriBuilderFactory ubf = new DefaultUriBuilderFactory();
-		UriBuilder ub = ubf.builder()
-				.scheme(scheme)
-				.host(host).port(port);
-		for (String path: paths) {
-			ub = ub.path(path);
-		}
-		return ub.build();
-	}
+    private URI uri(String scheme, String host, int port, String ...paths) {
+        UriBuilderFactory ubf = new DefaultUriBuilderFactory();
+        UriBuilder ub = ubf.builder().scheme(scheme).host(host).port(port);
+        for (String path: paths) {
+            ub = ub.path(path);
+        }
+        return ub.build();
+    }
 
-	private RequestEntity<Void> get(String scheme, String host, int port, String path) {
-		URI uri = uri(scheme, host,port, path);
-		var peticion = RequestEntity.get(uri)
-				.accept(MediaType.APPLICATION_JSON)
-				.build();
-		return peticion;
-	}
+    private RequestEntity<Void> get(String scheme, String host, int port, String path) {
+        URI uri = uri(scheme, host,port, path);
+        var peticion = RequestEntity.get(uri).accept(MediaType.APPLICATION_JSON).build();
+        return peticion;
+    }
 
-	private RequestEntity<Void> delete(String scheme, String host, int port, String path) {
-		URI uri = uri(scheme, host,port, path);
-		var peticion = RequestEntity.delete(uri)
-				.build();
-		return peticion;
-	}
+    private RequestEntity<Void> delete(String scheme, String host, int port, String path) {
+        URI uri = uri(scheme, host,port, path);
+        var peticion = RequestEntity.delete(uri).build();
+        return peticion;
+    }
 
-	private <T> RequestEntity<T> post(String scheme, String host, int port, String path, T object) {
-		URI uri = uri(scheme, host,port, path);
-		var peticion = RequestEntity.post(uri)
-				.contentType(MediaType.APPLICATION_JSON)
-				.body(object);
-		return peticion;
-	}
+    private <T> RequestEntity<T> post(String scheme, String host, int port, String path, T object) {
+        URI uri = uri(scheme, host,port, path);
+        var peticion = RequestEntity.post(uri).contentType(MediaType.APPLICATION_JSON).body(object);
+        return peticion;
+    }
 
-	private <T> RequestEntity<T> put(String scheme, String host, int port, String path, T object) {
-		URI uri = uri(scheme, host,port, path);
-		var peticion = RequestEntity.put(uri)
-				.contentType(MediaType.APPLICATION_JSON)
-				.body(object);
-		return peticion;
-	}
+    private <T> RequestEntity<T> put(String scheme, String host, int port, String path, T object) {
+        URI uri = uri(scheme, host,port, path);
+        var peticion = RequestEntity.put(uri).contentType(MediaType.APPLICATION_JSON).body(object);
+        return peticion;
+    }
 
+    @Nested
+    @DisplayName("cuando la base de datos está vacía")
+    public class BaseDatosVacia {
 
+        @Test
+        @DisplayName("error al obtener un evento")
+        public void errorEventoRepository() {
+			var peticion = get("http", "localhost", port, "/calendario/1/1");
 
+			var respuesta = restTemplate.exchange(peticion,new ParameterizedTypeReference<Evento>(){});
 
-	@Nested
-	@DisplayName("cuando la base de datos está vacía")
-	public class BaseDatosVacia {
+			assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
 
-		@Test
-		@DisplayName("error al obtener un ingrediente concreto")
-		public void errorEventoRepository() {
-			assertThat(true);
-		}
-	}
+        }
+    }
 }
