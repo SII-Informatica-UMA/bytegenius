99b35320f0829ecb02502d2a9276842a77824acc
Pablo6rubio <pablomunoz@uma.es>
2024-05-21 12:45:28 +0200

    Añado clase GlobalExceptionHandler.java para controlar errores

---
+++ ByteGenius T2/src/main/java/ByteGenius/tarea2/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package ByteGenius.tarea2.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ElementoNoExisteException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void handleElementoNoExisteException(ElementoNoExisteException ex) {
+        // Aquí puedes agregar lógica adicional, como logging, si es necesario
+    }
+}
